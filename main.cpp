a8c88f91db15fce228c8324dc661fc0b16e57286
Sujyot <70472979+sujyotraut@users.noreply.github.com>
2024-11-28 15:01:10 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) { glViewport(0, 0, width, height); }
+
+void process_input(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
+int main() {
+  // Initialize glfw
+  if (!glfwInit()) {
+    std::cout << "Failed to initialize glfw" << std::endl;
+    return 0;
+  }
+
+  // Use OpenGL 3.3
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  /* OpenGL provides different profiles that define a set of features and capabilities.
+   * The most common profiles are the core profile and the compatibility profile.
+   * Core profile: The core profile defines the essential features of OpenGL.
+   * It provides a minimal set of functionality and is recommended for modern applications.
+   * Compatibility profile: The compatibility profile includes all the features of the core profile, as well as
+   * additional features that are deprecated or removed in the core profile.
+   * It is recommended for applications that need to support older hardware or legacy features. */
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create glfw window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  /* When you create a GLFW window, it also creates an OpenGL context associated with that window.
+   * However, this context is not automatically made current, meaning that OpenGL commands will not be directed to this
+   * context by default. Calling `glfwMakeContextCurrent(window)` means that any subsequent OpenGL commands will be
+   * executed on this context, and will be rendered to the specified window. If you have multiple windows, each with its
+   * own OpenGL context, you need to make the correct context current before issuing OpenGL commands. This ensures that
+   * the commands are executed on the correct window. */
+  glfwMakeContextCurrent(window);
+
+  /* `glfwGetProcAddress("glClear")` will return an address to an OpenGL `glClear()` function. */
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // Set window resize callback
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    /* When an event occurs, GLFW stores it in an internal event queue.
+     * `glfwPollEvents()` is used to process the events in the queue.
+     * Here's what `glfwPollEvents()` does:
+     * 1. Check the event queue: GLFW checks the event queue for pending events.
+     * 2. Process each event: For each event in the queue, GLFW calls the corresponding callback function.
+     * 3. Clear the event queue: After processing all events, GLFW clears the event queue. */
+    glfwPollEvents();
+    process_input(window);
+
+    glClearColor(.196f, .196f, .196f, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* In OpenGL, a window typically has two buffers: the front buffer and the back buffer.
+     * The front buffer is the buffer that is currently being displayed on the screen.
+     * The back buffer is the buffer that is being drawn to.
+     * When we call `glfwSwapBuffers(window)` the back buffer becomes the front buffer,
+     * and the front buffer becomes the back buffer. */
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
+  return 0;
+}
